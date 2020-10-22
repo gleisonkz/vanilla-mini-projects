742ab1213d530d82927654de70d84636ae1d961f
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-10-22 19:02:52 -0300

    refactor: adding transiton on background-color.

--- colorful-click-button/script.js
+++ colorful-click-button/script.js
@@ -1,7 +1,9 @@
 const colors = ["green", "red", "blue"];
 const $button = document.querySelector(".btn");
+$button.style.backgroundColor = colors[0];
 $button.addEventListener("click", () => {
   currentBgColor = $button.style.backgroundColor;
+  console.log(currentBgColor);
   let currentIndex = colors.indexOf(currentBgColor);
   let nextIndex = ++currentIndex % colors.length;
   $button.style.backgroundColor = colors[nextIndex];
