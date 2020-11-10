877a73193680c9f6627d13e73ae37b8c605e8f0e
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-11-10 08:39:07 -0300

    docs: removing test log.

--- projects/colorful-click-button/script.js
+++ projects/colorful-click-button/script.js
@@ -2,8 +2,7 @@ const colors = ["green", "red", "blue"];
 const $button = document.querySelector(".btn");
 $button.style.backgroundColor = colors[0];
 $button.addEventListener("click", () => {
-  currentBgColor = $button.style.backgroundColor;
-  console.log(currentBgColor);
+  currentBgColor = $button.style.backgroundColor;  
   let currentIndex = colors.indexOf(currentBgColor);
   let nextIndex = ++currentIndex % colors.length;
   $button.style.backgroundColor = colors[nextIndex];
