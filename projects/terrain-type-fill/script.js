3ecaec50f5dfa349fc1329a247c4ffd932914ad0
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-11-10 23:32:39 -0300

    ✨ feat: debouncing function.

--- projects/terrain-type-fill/script.js
+++ projects/terrain-type-fill/script.js
@@ -6,21 +6,54 @@ const areas = [$squaresArea1, $squaresArea2, $squaresArea3];
 const $inputs = document.querySelectorAll(".form__control");
 $inputs.forEach(($input) =>
   $input.addEventListener("keyup", ($event) => {
-    const defaultBackground = "var(--base-bg)";
+    const defaultBackground = "url(images/default.jpg)";
+
     const currentValue = $event.target.value.toLowerCase();
     const areaID = $input.getAttribute("data-area");
     const terrains = [
-      { name: "rock", background: "url(./images/rock.jpg)" },
-      { name: "desert", background: "url(./images/desert.jpg)" },
-      { name: "grass", background: "url(./images/grass.jpg)" },
-      { name: "water", background: "url(./images/water.jpg)" },
-      { name: "ground", background: "url(./images/ground.jpg)" },
-      { name: "forest", background: "url(./images/forest.jpg)" },
-      { name: "lava", background: "url(./images/lava.jpg)" },
+      { name: "rock", background: "url(images/rock.jpg)" },
+      { name: "desert", background: "url(images/desert.jpg)" },
+      { name: "grass", background: "url(images/grass.jpg)" },
+      { name: "water", background: "url(images/water.jpg)" },
+      { name: "ground", background: "url(images/ground.jpg)" },
+      { name: "forest", background: "url(images/forest.jpg)" },
+      { name: "lava", background: "url(images/lava.jpg)" },
     ];
     const terrain = terrains.find((terrain) => terrain.name === currentValue);
     const currentSquaresArea = areas[areaID - 1];
 
-    currentSquaresArea.forEach(($square) => ($square.style.background = terrain?.background || defaultBackground));
+    currentSquaresArea.forEach(($square) => {
+      const nextBackground = terrain?.background || defaultBackground;
+      nextBackground !== getElementBackgroundUrl($square) &&
+        debounce(() => applyBackgroundTransitionEffect($square, nextBackground), 600)();
+    });
   })
 );
+
+function applyBackgroundTransitionEffect($square, background) {
+  $square.style.opacity = "0";
+  setTimeout(() => {
+    $square.style.background = background;
+    $square.style.opacity = "1";
+  }, 400);
+}
+
+function getElementBackgroundUrl($element) {
+  let url = window.getComputedStyle($element, false).backgroundImage;
+  url = "url(" + url.split("/").splice(5, 2).join("/").replace('.jpg"', ".jpg");
+  return url;
+}
+
+function debounce(func, timeout) {
+  let timer;
+
+  return (...args) => {
+    const next = () => func(...args);
+
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(next, timeout > 0 ? timeout : 300);
+  };
+}
