44147af511c3cc1c6690b5355a2c9ca99c697df1
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-11-11 08:25:58 -0300

    📦 refactor: remove unnecessary declaration on debounce function.

--- projects/terrain-type-fill/script.js
+++ projects/terrain-type-fill/script.js
@@ -1,43 +1,40 @@
-// const $squaresArea1 = document.querySelectorAll(".board__square[data-area='1']");
-// const $squaresArea2 = document.querySelectorAll(".board__square[data-area='2']");
-// const $squaresArea3 = document.querySelectorAll(".board__square[data-area='3']");
-// const areas = [$squaresArea1, $squaresArea2, $squaresArea3];
 const $areas = [...document.querySelectorAll(".board__square")];
-console.log(
-  "areas: ",
-  $areas.filter(($area) => $area.getAttribute("data-area") === "3")
-);
+const defaultBackground = "url(images/default.jpg)";
+const terrains = [
+  { name: "rock", background: "url(images/rock.jpg)" },
+  { name: "desert", background: "url(images/desert.jpg)" },
+  { name: "grass", background: "url(images/grass.jpg)" },
+  { name: "water", background: "url(images/water.jpg)" },
+  { name: "ground", background: "url(images/ground.jpg)" },
+  { name: "forest", background: "url(images/forest.jpg)" },
+  { name: "lava", background: "url(images/lava.jpg)" },
+  { name: "default", background: "url(images/default.jpg)" },
+];
 
 const $inputs = document.querySelectorAll(".form__control");
 $inputs.forEach(($input) =>
-  $input.addEventListener("keyup", ($event) => {
-    const defaultBackground = "url(images/default.jpg)";
-
-    const currentInputValue = $event.target.value.toLowerCase();
-    const areaID = $input.getAttribute("data-area");
-
-    const terrains = [
-      { name: "rock", background: "url(images/rock.jpg)" },
-      { name: "desert", background: "url(images/desert.jpg)" },
-      { name: "grass", background: "url(images/grass.jpg)" },
-      { name: "water", background: "url(images/water.jpg)" },
-      { name: "ground", background: "url(images/ground.jpg)" },
-      { name: "forest", background: "url(images/forest.jpg)" },
-      { name: "lava", background: "url(images/lava.jpg)" },
-    ];
-    const terrain = terrains.find((terrain) => terrain.name === currentInputValue);
-    const $currentSquaresArea = $areas.filter(($area) => $area.getAttribute("data-area") === areaID);
-    console.log($currentSquaresArea);
-
-    $currentSquaresArea.forEach(($square) => {
-      const nextBackground = terrain?.background || defaultBackground;
-      nextBackground !== getElementBackgroundUrl($square) &&
-        debounce(() => applyBackgroundTransitionEffect($square, nextBackground), 600)();
-    });
-  })
+  $input.addEventListener(
+    "keyup",
+    debounce(() => changeBackground($input))
+  )
 );
 
+function changeBackground($input) {
+  const currentInputValue = $input.value.toLowerCase();
+  const areaID = $input.getAttribute("data-area");
+
+  const terrain = terrains.find((terrain) => terrain.name === currentInputValue);
+  const nextBackground = terrain?.background || defaultBackground;
+
+  const $currentSquaresArea = $areas.filter(($area) => $area.getAttribute("data-area") === areaID);
+
+  $currentSquaresArea.forEach(($square) => {
+    nextBackground !== getElementBackgroundUrl($square) && applyBackgroundTransitionEffect($square, nextBackground);
+  });
+}
+
 function applyBackgroundTransitionEffect($square, background) {
+  console.log("applyBackgroundTransitionEffect");
   $square.style.opacity = "0";
   setTimeout(() => {
     $square.style.background = background;
@@ -51,16 +48,14 @@ function getElementBackgroundUrl($element) {
   return url;
 }
 
-function debounce(func, timeout) {
+function debounce(func, timeout = 300) {
   let timer;
 
   return (...args) => {
-    const next = () => func(...args);
-
     if (timer) {
       clearTimeout(timer);
     }
 
-    timer = setTimeout(next, timeout > 0 ? timeout : 300);
+    timer = setTimeout(() => func(...args), timeout);
   };
 }
